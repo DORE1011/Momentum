362ccad1b8f358a852184349166b39e3584ea302
DORE1011 <eunjkim2309@gmail.com>
2022-03-21 06:15:13 +0900

    second

--- js/login.js
+++ js/login.js
@@ -1,10 +1,10 @@
 const loginForm = document.querySelector(".login_form");
 const loginInput = document.querySelector(".login_form input:first-child")
 const welcomeContainer = document.querySelector("#welcome");
-const welcome = document.querySelector("#welcome h1");
+const welcome = document.querySelector("h1");
+// const loginContainer = document.querySelector(".login");
 
 const ON = "on";
-const HIDDEN = "hidden";
 const USERNAME_KEY = "username";
 
 function LoginSubmit(event) {
@@ -13,13 +13,13 @@ function LoginSubmit(event) {
 
     localStorage.setItem(USERNAME_KEY, username);
     loginForm.classList.add(ON);
+    // loginContainer.classList.add(ON);
     paintGreetings(username);
 }
 
 function paintGreetings(username) {
     welcome.innerText = `Hello ${username}!`;
     welcome.classList.remove(ON);
-    welcomeContainer.classList.add(HIDDEN);
 }
 
 const saveUsername = localStorage.getItem(USERNAME_KEY);
